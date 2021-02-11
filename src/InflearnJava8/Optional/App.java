6176ad9837748ece74dc803390c02a26ba6676ac
Jungmin-Seo <soato1405@naver.com>
2021-02-11 15:25:20 +0900

    Optional

--- src/InflearnJava8/Optional/App.java
+++ src/InflearnJava8/Optional/App.java
@@ -21,8 +21,34 @@ public class App {
         // System.out.println(studyDuration);
         Optional<Progress> progress = spring_boot.getProgress();
 
+        Optional<OnlineClass> optional = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("jpa"))
+                .findFirst();
 
+        optional.ifPresent(oc-> System.out.println(oc.getTitle()));
+        OnlineClass onlineClass = optional.orElse(createNewClasss()); // 무조건 createNewClasss 메소드를 실행은 함
+        System.out.println(onlineClass.getTitle()); // 단 optional 이 널이 아니면 값을 바꾸진 않음
 
+        OnlineClass onlineClass2 = optional.orElseGet(App::createNewClasss); // optionl 에 값이 없을때만 실행을 함
+        System.out.println(onlineClass.getTitle());
+
+        // OnlineClass onlineClass1 = optional.orElseThrow(IllegalArgumentException::new);
+        // System.out.println(onlineClass1.getTitle());
+
+        Optional<OnlineClass> onlineClass3 = optional
+                .filter(oc -> !oc.isClosed());
+        System.out.println(onlineClass3.isEmpty());
+
+        Optional<Integer> integer = optional.map(OnlineClass::getId);
+        System.out.println(integer.isPresent());
+
+        Optional<Progress> progressMap = optional.flatMap(OnlineClass::getProgress);
+
+    }
+
+    private static OnlineClass createNewClasss() {
+        System.out.println("creating new online class");
+        return new OnlineClass(10, "New class", false);
     }
 
 }
